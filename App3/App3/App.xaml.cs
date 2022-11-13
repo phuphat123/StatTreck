5f04e6681aca012c0a311575ae643ed3daa5c916
phuph <phuph@DESKTOP-9CD43OC>
2022-11-13 06:07:29 +0000

    Add project files.

---
+++ App3/App3/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+[assembly: ExportFont("Clear.ttf")]
+
+namespace App3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
