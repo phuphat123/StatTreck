78dc4f3d4ec42d9c20cc473a89263fc7e911a65e
phuphat123 <54221123+phuphat123@users.noreply.github.com>
2023-01-13 06:40:50 +0800

    Interaction with buttons

--- App3/App3/MainPage.xaml.cs
+++ App3/App3/MainPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -13,13 +15,40 @@ namespace App3
         public MainPage()
         {
             InitializeComponent();
-            
+
+            GPS.Clicked += Button_Clicked;
+            Motion.Clicked += Button_Clicked;
 
         }
 
         private void SwipeGestureRecognizer_Swiped(object sender, SwipedEventArgs e)
         {
             Navigation.PushAsync(new Data());
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Button Clicked!");
+            Data emptyPage = new Data();
+            Button button = sender as Button;
+            if (button == GPS)
+            {
+                Debug.WriteLine("GPS Clicked");
+                StackLayout s = new StackLayout();
+                s.Children.Add(new Label {Text = "You've clicked GPS!" });
+                emptyPage.Content = s;
+                Navigation.PushAsync(emptyPage);
+
+
+            }
+            else if (button == Motion) {
+                Debug.WriteLine("Motion Clicked");
+                StackLayout s = new StackLayout();
+                s.Children.Add(new Label { Text = "You've clicked Motion!" });
+                emptyPage.Content = s;
+                Navigation.PushAsync(emptyPage);
+            }
+
         }
     }
 }
