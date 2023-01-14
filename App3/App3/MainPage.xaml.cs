bc5d277cf2b9f8a52dfd1afdc650e23cd3b7cb49
phuphat123 <54221123+phuphat123@users.noreply.github.com>
2023-01-14 11:40:03 +0800

    Geolocation prototype half-complete.
    needs to track more locations passively and store externally.

--- App3/App3/MainPage.xaml.cs
+++ App3/App3/MainPage.xaml.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Essentials;
 using System.Data;
+using Xamarin.Forms.Maps;
 
 namespace App3
 {
@@ -22,9 +23,10 @@ namespace App3
             GPS.Clicked += Button_Clicked;
             Motion.Clicked += Button_Clicked;
 
-            
-            
-            
+            pin = new List<Pin>();
+
+
+
 
         }
 
@@ -32,14 +34,20 @@ namespace App3
         {
             Navigation.PushAsync(new Data());
         }
+        double locationLat, locationLng;
+        List<Pin> pin;
+
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
+            
+
             Debug.WriteLine("Button Clicked!");
             Data emptyPage = new Data();
             Button button = sender as Button;
             if (button == GPS)
             {
+                
                 Debug.WriteLine("GPS Clicked");
                 StackLayout s = new StackLayout();
                 s.Children.Add(new Label { Text = "You've clicked GPS!" });
@@ -61,17 +69,33 @@ namespace App3
                         {
                             Debug.WriteLine(location.Latitude + "" + location.Altitude);
                             locationStr += "Latitude: " + location.Latitude + " Longitude: " + location.Longitude;
+                            locationLat = location.Latitude;
+                            locationLng = location.Longitude;
                         }
                     }
                     catch (FeatureNotSupportedException fnsEx) { Debug.WriteLine("Feature not supported." + fnsEx.Message); }
                     catch (PermissionException pEx) { Debug.WriteLine("Feature not supported." + pEx.Message); }
                     catch (Exception ex) { Debug.WriteLine("Feature not supported." + ex.Message); }
+
+
                 }
-
-
-
                 
+                
+                var map = new Xamarin.Forms.Maps.Map();
+                map.HeightRequest = 100;
+                map.WidthRequest = 200;
+                map.MapType = MapType.Street;
+                map.IsShowingUser = false;
+                var position = new Position(locationLat,locationLng);
+                map.MoveToRegion(MapSpan.FromCenterAndRadius(position, Distance.FromKilometers(0.5)));
+                
+                map.Pins.Add(new Pin { Type = PinType.Generic, Position = position, Label = "Current Location" });
+                
+
+
+
                 s.Children.Add(new Label { Text = locationStr});
+                s.Children.Add(map);
                 emptyPage.Content = s;
                 await Navigation.PushAsync(emptyPage);
             }
